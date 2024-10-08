48cb145e676ee3dafda2cca71f8a88ed7028abb0
public-release <as-public-release@janestreet.com>
2024-10-08 20:31:00 +0100

    v0.18~preview.130.00+55

--- test/test_bigstring.ml
+++ test/test_bigstring.ml
@@ -163,45 +163,45 @@ module Local = struct
   let unsafe_get_string = unsafe_get_string
 end
 
-let%test_module "truncating setters (should end in [_trunc] or begin with [unsafe_])" =
-  (module struct
-    let test setters z =
-      try_setters z setters
-      |> List.iter ~f:(fun t ->
-        Or_error.ok_exn t |> List.iter ~f:(printf "%x ");
-        printf "; ")
-    ;;
+module%test
+  [@name "truncating setters (should end in [_trunc] or begin with [unsafe_])"] _ =
+struct
+  let test setters z =
+    try_setters z setters
+    |> List.iter ~f:(fun t ->
+      Or_error.ok_exn t |> List.iter ~f:(printf "%x ");
+      printf "; ")
+  ;;
 
-    let%expect_test "all word sizes" =
-      test [ (fun buf ~pos value -> unsafe_set_int8 buf ~pos value) ] 0x9080;
-      [%expect {| 80 0 0 0 0 0 0 0 ; |}];
-      test [ (fun buf ~pos value -> unsafe_set_uint8 buf ~pos value) ] (-1);
-      [%expect {| ff 0 0 0 0 0 0 0 ; |}];
+  let%expect_test "all word sizes" =
+    test [ (fun buf ~pos value -> unsafe_set_int8 buf ~pos value) ] 0x9080;
+    [%expect {| 80 0 0 0 0 0 0 0 ; |}];
+    test [ (fun buf ~pos value -> unsafe_set_uint8 buf ~pos value) ] (-1);
+    [%expect {| ff 0 0 0 0 0 0 0 ; |}];
+    test
+      [ (fun buf ~pos value -> unsafe_set_int16_le buf ~pos value)
+      ; (fun buf ~pos value -> unsafe_set_int16_be buf ~pos value)
+      ]
+      0x90_8070;
+    [%expect {| 70 80 0 0 0 0 0 0 ; 80 70 0 0 0 0 0 0 ; |}];
+    test [ unsafe_set_uint16_le; unsafe_set_uint16_be ] (-1);
+    [%expect {| ff ff 0 0 0 0 0 0 ; ff ff 0 0 0 0 0 0 ; |}];
+    test [ unsafe_set_uint32_le; unsafe_set_uint32_be ] (-1);
+    [%expect {| ff ff ff ff 0 0 0 0 ; ff ff ff ff 0 0 0 0 ; |}];
+    test [ unsafe_set_uint64_le; unsafe_set_uint64_be ] (-1);
+    [%expect {| ff ff ff ff ff ff ff ff ; ff ff ff ff ff ff ff ff ; |}]
+  ;;
+
+  let%expect_test (_ [@tags "64-bits-only"]) =
+    Option.iter (Int64.to_int 0x90_8070_6050L) ~f:(fun z ->
       test
-        [ (fun buf ~pos value -> unsafe_set_int16_le buf ~pos value)
-        ; (fun buf ~pos value -> unsafe_set_int16_be buf ~pos value)
+        [ (fun buf ~pos value -> unsafe_set_int32_le buf ~pos value)
+        ; (fun buf ~pos value -> unsafe_set_int32_be buf ~pos value)
         ]
-        0x90_8070;
-      [%expect {| 70 80 0 0 0 0 0 0 ; 80 70 0 0 0 0 0 0 ; |}];
-      test [ unsafe_set_uint16_le; unsafe_set_uint16_be ] (-1);
-      [%expect {| ff ff 0 0 0 0 0 0 ; ff ff 0 0 0 0 0 0 ; |}];
-      test [ unsafe_set_uint32_le; unsafe_set_uint32_be ] (-1);
-      [%expect {| ff ff ff ff 0 0 0 0 ; ff ff ff ff 0 0 0 0 ; |}];
-      test [ unsafe_set_uint64_le; unsafe_set_uint64_be ] (-1);
-      [%expect {| ff ff ff ff ff ff ff ff ; ff ff ff ff ff ff ff ff ; |}]
-    ;;
-
-    let%expect_test (_ [@tags "64-bits-only"]) =
-      Option.iter (Int64.to_int 0x90_8070_6050L) ~f:(fun z ->
-        test
-          [ (fun buf ~pos value -> unsafe_set_int32_le buf ~pos value)
-          ; (fun buf ~pos value -> unsafe_set_int32_be buf ~pos value)
-          ]
-          z);
-      [%expect {| 50 60 70 80 0 0 0 0 ; 80 70 60 50 0 0 0 0 ; |}]
-    ;;
-  end)
-;;
+        z);
+    [%expect {| 50 60 70 80 0 0 0 0 ; 80 70 60 50 0 0 0 0 ; |}]
+  ;;
+end
 
 let getter_t ~first_byte = init 8 ~f:(fun i -> i + first_byte |> Char.of_int_exn)
 let get_int64_le_trunc = get_int64_le_trunc
@@ -209,102 +209,102 @@ let get_int64_be_trunc = get_int64_be_trunc
 let unsafe_get_int64_le_trunc = unsafe_get_int64_le_trunc
 let unsafe_get_int64_be_trunc = unsafe_get_int64_be_trunc
 
-let%test_module "truncating getters (should end in [_trunc] or begin with [unsafe_])" =
-  (module struct
-    let test getter =
-      List.iter
-        [ 0x81 (* positive if top bit truncated *)
-        ; 0xc1 (* negative if top bit truncated *)
-        ]
-        ~f:(fun first_byte ->
-          let i = getter (getter_t ~first_byte) ~pos:0 in
-          (* Signed hex is not clear; make sure the hex is unsigned.  Include the signed
+module%test
+  [@name "truncating getters (should end in [_trunc] or begin with [unsafe_])"] _ =
+struct
+  let test getter =
+    List.iter
+      [ 0x81 (* positive if top bit truncated *)
+      ; 0xc1 (* negative if top bit truncated *)
+      ]
+      ~f:(fun first_byte ->
+        let i = getter (getter_t ~first_byte) ~pos:0 in
+        (* Signed hex is not clear; make sure the hex is unsigned.  Include the signed
              decimal form mainly to indicate the sign. *)
-          printf !"0x%x (= %d)\n" i i)
-    ;;
+        printf !"0x%x (= %d)\n" i i)
+  ;;
 
-    let%expect_test ("63-bit int" [@tags "64-bits-only"]) =
-      test get_int64_le_trunc;
-      [%expect
-        {|
-        0x887868584838281 (= 614607782171345537)
-        0x48c7c6c5c4c3c2c1 (= -3978993193046523199)
-        |}];
-      test get_int64_be_trunc;
-      [%expect
-        {|
-        0x182838485868788 (= 108793946209421192)
-        0x41c2c3c4c5c6c7c8 (= -4484807029008447544)
-        |}];
-      test unsafe_get_int64_le_trunc;
-      [%expect
-        {|
-        0x887868584838281 (= 614607782171345537)
-        0x48c7c6c5c4c3c2c1 (= -3978993193046523199)
-        |}];
-      test unsafe_get_int64_be_trunc;
-      [%expect
-        {|
-        0x182838485868788 (= 108793946209421192)
-        0x41c2c3c4c5c6c7c8 (= -4484807029008447544)
-        |}]
-    ;;
+  let%expect_test ("63-bit int" [@tags "64-bits-only"]) =
+    test get_int64_le_trunc;
+    [%expect
+      {|
+      0x887868584838281 (= 614607782171345537)
+      0x48c7c6c5c4c3c2c1 (= -3978993193046523199)
+      |}];
+    test get_int64_be_trunc;
+    [%expect
+      {|
+      0x182838485868788 (= 108793946209421192)
+      0x41c2c3c4c5c6c7c8 (= -4484807029008447544)
+      |}];
+    test unsafe_get_int64_le_trunc;
+    [%expect
+      {|
+      0x887868584838281 (= 614607782171345537)
+      0x48c7c6c5c4c3c2c1 (= -3978993193046523199)
+      |}];
+    test unsafe_get_int64_be_trunc;
+    [%expect
+      {|
+      0x182838485868788 (= 108793946209421192)
+      0x41c2c3c4c5c6c7c8 (= -4484807029008447544)
+      |}]
+  ;;
 
-    let%expect_test ("31-bit int" [@tags "wasm-only"]) =
-      test get_int64_le_trunc;
-      [%expect
-        {|
-        0x4838281 (= 75727489)
-        0x44c3c2c1 (= -993803583)
-        |}];
-      test get_int64_be_trunc;
-      [%expect
-        {|
-        0x5868788 (= 92702600)
-        0x45c6c7c8 (= -976828472)
-        |}];
-      test unsafe_get_int64_le_trunc;
-      [%expect
-        {|
-        0x4838281 (= 75727489)
-        0x44c3c2c1 (= -993803583)
-        |}];
-      test unsafe_get_int64_be_trunc;
-      [%expect
-        {|
-        0x5868788 (= 92702600)
-        0x45c6c7c8 (= -976828472)
-        |}]
-    ;;
+  let%expect_test ("31-bit int" [@tags "wasm-only"]) =
+    test get_int64_le_trunc;
+    [%expect
+      {|
+      0x4838281 (= 75727489)
+      0x44c3c2c1 (= -993803583)
+      |}];
+    test get_int64_be_trunc;
+    [%expect
+      {|
+      0x5868788 (= 92702600)
+      0x45c6c7c8 (= -976828472)
+      |}];
+    test unsafe_get_int64_le_trunc;
+    [%expect
+      {|
+      0x4838281 (= 75727489)
+      0x44c3c2c1 (= -993803583)
+      |}];
+    test unsafe_get_int64_be_trunc;
+    [%expect
+      {|
+      0x5868788 (= 92702600)
+      0x45c6c7c8 (= -976828472)
+      |}]
+  ;;
 
-    let%expect_test ("32-bit int" [@tags "js-only", "no-wasm"]) =
-      test get_int64_le_trunc;
-      [%expect
-        {|
-        0x84838281 (= -2071756159)
-        0xc4c3c2c1 (= -993803583)
-        |}];
-      test get_int64_be_trunc;
-      [%expect
-        {|
-        0x85868788 (= -2054781048)
-        0xc5c6c7c8 (= -976828472)
-        |}];
-      test unsafe_get_int64_le_trunc;
-      [%expect
-        {|
-        0x84838281 (= -2071756159)
-        0xc4c3c2c1 (= -993803583)
-        |}];
-      test unsafe_get_int64_be_trunc;
-      [%expect
-        {|
-        0x85868788 (= -2054781048)
-        0xc5c6c7c8 (= -976828472)
-        |}]
-    ;;
-  end)
-;;
+  let%expect_test ("32-bit int" [@tags "js-only", "no-wasm"]) =
+    test get_int64_le_trunc;
+    [%expect
+      {|
+      0x84838281 (= -2071756159)
+      0xc4c3c2c1 (= -993803583)
+      |}];
+    test get_int64_be_trunc;
+    [%expect
+      {|
+      0x85868788 (= -2054781048)
+      0xc5c6c7c8 (= -976828472)
+      |}];
+    test unsafe_get_int64_le_trunc;
+    [%expect
+      {|
+      0x84838281 (= -2071756159)
+      0xc4c3c2c1 (= -993803583)
+      |}];
+    test unsafe_get_int64_be_trunc;
+    [%expect
+      {|
+      0x85868788 (= -2054781048)
+      0xc5c6c7c8 (= -976828472)
+      |}]
+  ;;
+end
 
 let get_int64_le_exn = get_int64_le_exn
 let get_int64_be_exn = get_int64_be_exn
@@ -707,30 +707,28 @@ let memcmp = memcmp
 let memcmp_bytes = memcmp_bytes
 let memcmp_string = memcmp_string
 
-let%test_module "basic memcmp" =
-  (module struct
-    let s1 = "221007247563588720"
-    let s2 = "1650905272620466461"
+module%test [@name "basic memcmp"] _ = struct
+  let s1 = "221007247563588720"
+  let s2 = "1650905272620466461"
 
-    let test_memcmp ~memcmp t1 t2 =
-      [%test_result: int] ~expect:0 (memcmp t1 ~pos1:0 t2 ~pos2:0 ~len:0);
-      [%test_pred: int] Int.is_positive (memcmp t1 ~pos1:0 t2 ~pos2:0 ~len:3);
-      [%test_pred: int] Int.is_negative (memcmp t1 ~pos1:0 t2 ~pos2:1 ~len:3)
-    ;;
+  let test_memcmp ~memcmp t1 t2 =
+    [%test_result: int] ~expect:0 (memcmp t1 ~pos1:0 t2 ~pos2:0 ~len:0);
+    [%test_pred: int] Int.is_positive (memcmp t1 ~pos1:0 t2 ~pos2:0 ~len:3);
+    [%test_pred: int] Int.is_negative (memcmp t1 ~pos1:0 t2 ~pos2:1 ~len:3)
+  ;;
 
-    let%expect_test "bigstring to bigstring" =
-      test_memcmp ~memcmp (of_string s1) (of_string s2)
-    ;;
+  let%expect_test "bigstring to bigstring" =
+    test_memcmp ~memcmp (of_string s1) (of_string s2)
+  ;;
 
-    let%expect_test "bigstring to bytes" =
-      test_memcmp ~memcmp:memcmp_bytes (of_string s1) (Bytes.of_string s2)
-    ;;
+  let%expect_test "bigstring to bytes" =
+    test_memcmp ~memcmp:memcmp_bytes (of_string s1) (Bytes.of_string s2)
+  ;;
 
-    let%expect_test "bigstring to string" =
-      test_memcmp ~memcmp:memcmp_string (of_string s1) s2
-    ;;
-  end)
-;;
+  let%expect_test "bigstring to string" =
+    test_memcmp ~memcmp:memcmp_string (of_string s1) s2
+  ;;
+end
 
 let memset = memset
 
